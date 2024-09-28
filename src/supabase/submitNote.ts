cde8622ee4076ce322f019529fdd3311ba78d753
c.mirkin <ciromirkin@gmail.com>
2024-09-28 11:37:47 -0300

    agrega: uso de uuid al crear una nueva nota

--- src/supabase/submitNote.ts
+++ src/supabase/submitNote.ts
@@ -1,12 +1,13 @@
 import { Note } from "@/store";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { v4 as uuidv4 } from 'uuid';
 
 export async function submitNote(note: Note)  {
     const supabase = createClientComponentClient();
 
     const { error } = await supabase
         .from("notes")
-        .upsert({ ...note })
+        .upsert({ ...note, id: uuidv4() })
         .select("*")
         .single()
 
