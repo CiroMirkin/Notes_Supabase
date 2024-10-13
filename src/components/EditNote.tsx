9832ea43cdbfe94b3bd993e7a0e8db81db5e08fb
c.mirkin <ciromirkin@gmail.com>
2024-10-13 16:51:12 -0300

    cambia: se ordeno el codigo del componente EditNote

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -34,32 +34,30 @@ export function EditNote({ note }: { note: Note}) {
                 style={{ backgroundColor: "#fff" }}
             >
                 <DialogHeader>
-                <DialogTitle>Edit Note</DialogTitle>
-                <DialogDescription>
-                    {
-                    "What do you really have to do? Think about it."
-                    }
-                </DialogDescription>
+                    <DialogTitle>Edit Note</DialogTitle>
+                    <DialogDescription>
+                        {
+                            "What do you really have to do? Think about it."
+                        }
+                    </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                    Note
-                    </Label>
-                    <Input
-                    id="edit_note"
-                    placeholder="Note..."
-                    className="col-span-3"
-                    value={noteText}
-                    onChange={(e) => setNoteText(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                    />
-                </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">Note</Label>
+                        <Input
+                            id="edit_note"
+                            placeholder="Note..."
+                            className="col-span-3"
+                            value={noteText}
+                            onChange={(e) => setNoteText(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                        />
+                    </div>
                 </div>
                 <DialogFooter>
-                <Button type="submit" onClick={handleSubmit}>
-                    Save change
-                </Button>
+                    <Button type="submit" onClick={handleSubmit}>
+                        Save change
+                    </Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
